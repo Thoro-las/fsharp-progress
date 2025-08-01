610dbd171f2188657f81face9b7f0d5e0eb391aa
Thoro-las <had.azouaou@gmail.com>
2025-08-01 15:46:30 +0100

    Refresh

--- Resources/spd.fsx
+++
@@ -1,8 +0,0 @@
-module SPD
-
-open System
-open System.IO
-
-let writeToFile (date: DateTime) (filename: string) (text: string) =
-    let path = sprintf "%s-%s.txt" (date.ToString "ddMMyy") filename
-    File.WriteAllText(path, text)
